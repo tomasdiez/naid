1c0a79a775246f1eec53ede595ed5e8897ac33fa
ginger <26461046+gingerchew@users.noreply.github.com>
2024-12-09 13:43:37 -0500

    Adds LICENSE

--- uno.config.ts
+++ uno.config.ts
@@ -4,7 +4,7 @@ export default defineConfig({
     transformers: import.meta.env.DEV ? [] : [transformerCompileClass()],
     shortcuts: [{
         wrapper: 'w-100% max-w-[100vw] md:max-w-[calc(100vw-100px)] xl:max-w-[1120px] mx-auto',
-        title: 'text-10 leading-[1.5] dark:c-gray-100',
+        title: 'text-7 lg:text-10 leading-[1.2] dark:c-gray-100',
         'project-title': 'text-7 9lg:text-8 leading-[1.1]',
         subtitle: 'text-6 c-gray-700 leading-[1.2] dark:c-gray-300',
         'subtitle-small': 'text-4 c-gray-700 leading-[1.3] dark:c-gray-300',
