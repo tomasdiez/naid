d5001429129fb40423c285867cd473fd5f05812d
Tomas Diez <tomasdiez@gmail.com>
2025-08-03 17:27:29 +0800

    my own

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,25 @@ import Unocss from 'unocss/astro';
 
 import icon from 'astro-icon';
 
+import db from '@astrojs/db';
+import node from '@astrojs/node';
+import studiocms from 'studiocms';
+
+import { defineStudioCMSConfig } from 'studiocms/config';
+import html from '@studiocms/html';
+import md from '@studiocms/md';
+
 // https://astro.build/config
 export default defineConfig({
-    integrations: [Unocss({ injectReset: true, injectEntry: true }), icon()]
+  site: 'http://localhost:4321/', 
+  output: 'server',
+  integrations: [Unocss({ injectReset: true, injectEntry: true }), icon(), db(), studiocms()],
+  plugins: [
+    html ,
+    md ,
+  ],
+
+  adapter: node({
+    mode: 'standalone'
+  })
 });
